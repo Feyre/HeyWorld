65725e141d52c8039108d2a06a7828f23d74f607
Lachlan Nicholson <lnich13@live.com.au>
2015-05-31 12:11:25 +1000

    Learning Java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String []args) {
+	   System.out.println("Hello World");
+	   System.out.println("number = " + foo(3));
+	}
+	
+	static int foo(int x) {
+		return (x*2);
+	}
+	
+	
+}
